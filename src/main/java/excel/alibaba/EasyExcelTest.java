fc1bceb8df83deaf0ad0980bbc09b48344ee39b1
manito-ming <1315915230@qq.com>
2019-12-04 16:44:36 +0800

    解决alibaba的easyexcel报错

--- src/main/java/excel/alibaba/EasyExcelTest.java
+++ src/main/java/excel/alibaba/EasyExcelTest.java
@@ -11,7 +11,20 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class EasyExcelTest {
-
+   /*  直接启动会报错，和poi的excel冲突
+   需要注释掉pom文件中的这两个依赖
+   <!--        poi excel -->
+        <dependency>
+            <groupId>org.apache.poi</groupId>
+            <artifactId>poi-ooxml</artifactId>
+            <version>3.16-beta2</version>
+        </dependency>
+        <dependency>
+            <groupId>xerces</groupId>
+            <artifactId>xercesImpl</artifactId>
+            <version>2.11.0</version>
+        </dependency>
+   * */
     public static void main(String[] args) throws IOException {
         InputStream in = new BufferedInputStream(new FileInputStream("C:\\Users\\mingzhihong\\Desktop\\map1.xlsx"));
         try {
